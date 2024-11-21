141b0fe31ba62a9101d771a5cb4ee0c221d00473
Patrick <99949502+zijunpeng@users.noreply.github.com>
2024-11-22 00:08:31 +0800

    Add files via upload

---
+++ poetize-im-ui/src/store/index.js
@@ -0,0 +1,19 @@
+import {createStore} from 'vuex'
+
+
+export default createStore({
+  state: {
+    currentUser: JSON.parse(localStorage.getItem("currentUser") || '{}'),
+    sysConfig: JSON.parse(localStorage.getItem("sysConfig") || '{}')
+  },
+  getters: {},
+  mutations: {
+    loadCurrentUser(state, user) {
+      state.currentUser = user;
+      localStorage.setItem("currentUser", JSON.stringify(user));
+    }
+  },
+  actions: {},
+  modules: {},
+  plugins: []
+})
